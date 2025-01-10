ce00d7beb15a7a9f954a16b25df52bdfe2c02d5f
dat-nguyen37 <dat958734@gmail.com>
2025-01-10 22:24:55 +0700

    commit

--- server/controller/Auth.js
+++ server/controller/Auth.js
@@ -1,5 +1,5 @@
 const User=require('../model/User')
-const bcrypt =require("bcrypt")
+const bcrypt =require("bcryptjs")
 const jwt =require('jsonwebtoken')
 const sendMail=require('../until/sendMail')
 
