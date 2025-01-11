610fb56d3698ef7dff383b4f196e9f515fe2f568
dat-nguyen37 <dat958734@gmail.com>
2025-01-11 09:49:05 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app=express()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://shop-fe.onrender.com/',
+        origin: 'https://shop-fe.onrender.com',
         credentials: true,
     },
 });
