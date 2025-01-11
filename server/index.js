a286750ccdf00b4735faccb6123ef51c1075962e
dat-nguyen37 <dat958734@gmail.com>
2025-01-11 21:01:05 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(cors(
 ))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
-    secret: 'your-secret-key',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
