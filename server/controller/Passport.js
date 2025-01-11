11ec44345e0f66b2093eaa02a8984b98d3894855
dat-nguyen37 <dat958734@gmail.com>
2025-01-11 17:19:16 +0700

    commit

--- server/controller/Passport.js
+++ server/controller/Passport.js
@@ -11,7 +11,6 @@ passport.use(new GoogleStrategy({
   },
   function (accessToken, refreshToken, profile, done) {
    // cb(undefined,profile)
-   console.log(profile);
     if(profile.id){
       User.findOne({email:profile.emails[0].value})
       .then((user)=>{
