9811b8b42b9c0a3f092a79590921b35f17135ffd
dat-nguyen37 <dat958734@gmail.com>
2025-01-12 13:08:57 +0700

    commit

--- client/src/components/seller/News/News.jsx
+++ client/src/components/seller/News/News.jsx
@@ -6,10 +6,56 @@ import { EuiPanel } from '@elastic/eui';
 export default function News() {
     const [value, setValue] = useState('');
     const reactQuillRef = useRef(null);
+    console.log(value);
 
     return (
         <EuiPanel style={{height:'calc(100vh - 3rem'}}>
-            <ReactQuill ref={reactQuillRef} theme="snow" value={value} onChange={setValue} />
+            <ReactQuill
+                ref={reactQuillRef}
+                theme="snow"
+                placeholder="Start writing..."
+                modules={{
+                    toolbar: {
+                    container: [
+                        [{ header: "1" }, { header: "2" }, { font: [] }],
+                        [{ size: [] }],
+                        ["bold", "italic", "underline", "strike", "blockquote"],
+                        [
+                        { list: "ordered" },
+                        { list: "bullet" },
+                        { indent: "-1" },
+                        { indent: "+1" },
+                        ],
+                        ["link", "image", "video"],
+                        ["code-block"],
+                        ["clean"],
+                    ],
+                    },
+                    clipboard: {
+                    matchVisual: false,
+                    },
+                }}
+                formats={[
+                    "header",
+                    "font",
+                    "size",
+                    "bold",
+                    "italic",
+                    "underline",
+                    "strike",
+                    "blockquote",
+                    "list",
+                    "bullet",
+                    "indent",
+                    "link",
+                    "image",
+                    "video",
+                    "code-block",
+                ]}
+                value={value}
+                onChange={setValue}
+                />
+
         </EuiPanel>
     );
 }
