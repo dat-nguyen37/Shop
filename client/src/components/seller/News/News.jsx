270f3dae27ce7f65032a4a250f86b8ba7502f7bb
dat-nguyen37 <dat958734@gmail.com>
2025-01-12 12:21:23 +0700

    commit

--- client/src/components/seller/News/News.jsx
+++ client/src/components/seller/News/News.jsx
@@ -1,26 +1,15 @@
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import React, { useState } from 'react';
 import { EuiPanel } from '@elastic/eui';
 
 export default function News() {
-    const [editorData, setEditorData] = useState("");
-
-    const handleEditorChange = (event, editor) => {
-        const data = editor.getData();
-        setEditorData(data);
-    };
+    const [value, setValue] = useState('');
+    const reactQuillRef = useRef(null);
 
     return (
         <EuiPanel style={{height:'calc(100vh - 3rem'}}>
-            <CKEditor
-                editor={ClassicEditor}
-                data={editorData}
-                onChange={handleEditorChange}
-                config={{
-                    toolbar: ['bold', 'italic', 'undo', 'redo']
-                }}
-            />
+            <ReactQuill ref={reactQuillRef} theme="snow" value={value} onChange={setValue} />
         </EuiPanel>
     );
 }
