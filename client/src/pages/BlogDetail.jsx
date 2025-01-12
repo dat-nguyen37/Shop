0dc5ceafcfa19416084e5e776c87ec64e0f05a20
dat-nguyen37 <dat958734@gmail.com>
2025-01-12 18:53:37 +0700

    commit

--- client/src/pages/BlogDetail.jsx
+++ client/src/pages/BlogDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { EuiAccordion, EuiAvatar, EuiBreadcrumbs, EuiFlexGroup, EuiFlexItem, EuiIcon, EuiImage, EuiLink, EuiListGroup, EuiListGroupItem, EuiPage, EuiPageSection, EuiPageSidebar, EuiPageTemplate, EuiPanel, EuiSpacer, EuiText } from '@elastic/eui'
 import { Outlet, useLocation, useOutletContext } from 'react-router-dom'
 import axios from '../axios'
+import DOMPurify from 'dompurify';
 
 export default function BlogDetail() {
     const [blogId,setBlogId]=useState(null)
@@ -30,7 +31,7 @@ export default function BlogDetail() {
         <EuiFlexGroup direction='column'>
             <EuiText><h3>{blog.title}</h3></EuiText>
             <EuiImage src={blog.image}/>
-            <EuiText>{blog.detail}</EuiText>
+            <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(blog?.detail) }} />
         </EuiFlexGroup>
     </EuiPageTemplate.Section>
   )
